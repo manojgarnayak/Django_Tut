153db41c2cfcbad7f7d346c2dc2454782eb48fef
manojgarnayak <manojgarnayak07@gmail.com>
2024-03-05 18:15:44 +0530

    files changed

--- project4/project4app/models.py
+++ project4/project4app/models.py
@@ -5,3 +5,6 @@ class LoginDetail(models.Model):
     fullname =  models.CharField(max_length=50)
     password =  models.CharField(max_length=50)
     email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
--- project4/project4app/views.py
+++ project4/project4app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from project4app.models import *
 # Create your views here.
 def home(request):
@@ -15,6 +15,17 @@ def register(request):
     return render(request, 'register.html')
 
 def login(request):
+    if request.method == 'POST':
+        users = LoginDetail.objects.all()
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        for user in users:
+            if user.email == email:
+                if user.password == password:
+                    return HttpResponse('Login Successfull')
+                return HttpResponse('Invalid Password')
+        else:
+            return HttpResponse('Invalid Email')
     return render(request, 'login.html')
 
 def contact(request):
