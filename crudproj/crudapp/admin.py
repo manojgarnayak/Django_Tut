8299805b25e409e426b10e27ce6752d4e397805a
manojgarnayak <manojgarnayak07@gmail.com>
2024-03-20 14:07:54 +0530

    "deleted"

--- crudproj/crudapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from crudapp.models import *
-# Register your models here.
-
-admin.site.register(TaskDetails)
--- crudproj/crudapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TaskDetails(models.Model):
-    title =  models.CharField(max_length=50)
-    description =  models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
